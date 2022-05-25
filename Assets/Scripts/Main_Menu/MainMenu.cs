745efcea3fb87fa40c54a051daecce7216b6a40e
Justin Pfeifler <jrpl101998@gmail.com>
2022-05-25 03:28:15 -0500

    Add disable coop on mobile

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+#if MOBILE_INPUT
+    private GameObject _coopButton;
+    void Start() {
+        _coopButton = GameObject.Find("Co_Op_Button");
+        _coopButton.SetActive(false);
+    }
+#endif
+
     public void NewSinglePlayer()
     {
         SceneManager.LoadScene("Single_Player");
